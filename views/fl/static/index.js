0d43af0e1a981ce49a9a3f0847d66d957b886cc1
Zabolekar <syekirin@gmail.com>
2019-08-18 19:18:09 +0200

    removing jquery completely

--- views/fl/static/index.js
+++ views/fl/static/index.js
@@ -1,13 +1,22 @@
-$(function () {
-   $("button").bind("click", function () {
-      $.get($SCRIPT_ROOT + '/on_click', {}, function (data) {
-         $("button").text(data);
-      });
-   });
-});
+let button = document.getElementById("button");
 
-var stream = new EventSource('/stream');
+button.onclick = function () {
+   var request = new XMLHttpRequest();
+
+   request.open('GET', $SCRIPT_ROOT + '/on_click', true);
+
+   request.onload = function() {
+      // we don't care about this.status
+      button.innerText = this.response;
+   };
+
+   // we don't care about connection errors either
+
+   request.send();
+}
+
+let stream = new EventSource('/stream');
 
 stream.onmessage = function (event) {
-   $("button").text(event.data);
+   button.innerText = event.data;
 };
